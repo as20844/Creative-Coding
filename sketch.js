bb92a5bd98e24da8d2a8ddc2a7114c6db9186484
as20844 <as20844@nyu.edu>
2026-03-06 12:21:31 +0800

    Change
    Change

--- sketch.js
+++ sketch.js
@@ -7,36 +7,98 @@ CCLaboratories Biodiversity Atlas
 
 let x;
 let y;
-let R = 100;
-let vNumber = 200;
-let smoothness = 0.5;
+let size = 10;
+let n = 10;
+let spread = 0
+let mouseholdtime = 0
 
 function setup() {
     let canvas = createCanvas(800, 500);
-    canvas.id("p5-canvas");
     canvas.parent("p5-canvas-container");
 }
 
 function draw() {
-    background(0);
+    if (mouseIsPressed) {
+        background("red");
+        mouseholdtime++;
+    } else {
+        background(0);
+        mouseholdtime = 0
+
+    }
+    drawCreature()
+}
+
+function drawCreature() {
+    if (mouseIsPressed) {
+        size += 0.2;
+    } else {
+        size = 10;
+    }
+
+    stroke(255);
+    noFill();
+    x = width / 2;
+    y = height / 2;
+    if (mouseIsPressed) {
+        x = x + random(-0.5, 1);
+        y = y + random(-0.5, 1);
+    }
+    if (mouseIsPressed) {
+        spread = 100;
+    } else {
+        spread *= 0.9;
+    }
+
+
+
+    fill(150, 0, 200);
     noStroke();
-    push();
-    translate(width / 2, height / 2);
-    rotate(sin(frameCount * 0.01));
+    ellipse(x - 55 + random(-spread, spread), y - 180 + random(-spread, spread), 30, 30);
+    ellipse(x + 100 + random(-spread, spread), y - 160 + random(-spread, spread), 30, 30);
+    ellipse(x - 130 + random(-spread, spread), y - 100 + random(-spread, spread), 30, 30);
+    ellipse(x - 130 + random(-spread, spread), y + 150 + random(-spread, spread), 30, 30);
+    ellipse(x + 140 + random(-spread, spread), y + 80 + random(-spread, spread), 30, 30);
+    ellipse(x + 50 + random(-spread, spread), y + 130 + random(-spread, spread), 30, 30);
+
+    if (mouseholdtime > 180) {
+        fill(0, 255, 0);
+    } else {
+        fill(150, 0, 200);
+    }
+
     beginShape();
-    for (let i = 0; i < TWO_PI; i += TWO_PI / vNumber) {
-        let offset = noise(
-            smoothness * sin(i),
-            smoothness * cos(i),
-            frameCount * 0.01
-        );
-        offset = map(offset, 0, 1, -R * 0.2, R * 0.2);
-        let radius = R + offset;
-        x = radius * cos(i);
-        y = radius * sin(i);
+    for (let i = 0; i < n; i++) {
+        let angle = map(i, 0, n, 0, 360);
+        let offset = map(i, 0, n, 0, 15);
+        let rad = 100 + sin(frameCount / 10 + offset) * size;
+        let x = width / 2 + cos(radians(angle)) * rad;
+        let y = height / 2 + sin(radians(angle)) * rad;
+        curveVertex(x, y);
+        angle = map(i, 0, n, 0, 360);
+        offset = map(i, 0, n, 0, 15);
+        rad = 100 + sin(frameCount / 30 + offset) * 20;
+        x = width / 2 + cos(radians(angle)) * rad;
+        y = height / 2 + sin(radians(angle)) * rad;
         curveVertex(x, y);
     }
     endShape(CLOSE);
-    pop();
+
+    fill(255);
+    noStroke();
+    ellipse(width / 2 - 20, height / 2 - 10, 15, 15);
+    ellipse(width / 2 + 20, height / 2 - 10, 15, 15);
+
+
+    stroke(200);
+    strokeWeight(4);
+
+    for (let i = 50; i < width; i += 40) {
+        line(i, 0, i, height);
+    }
+
+    line(0, 20, width, 20);
+    line(0, height - 20, width, height - 20);
+
 }
 
